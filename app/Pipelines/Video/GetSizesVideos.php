af0ada171260ca73dcaa80c74c51acf5c23a97c7
DmitriyLishikov <pain14141919@gmail.com>
2023-07-09 23:33:34 +0300

    fix

--- app/Pipelines/Video/GetSizesVideos.php
+++ app/Pipelines/Video/GetSizesVideos.php
@@ -3,26 +3,22 @@
 namespace App\Pipelines\Video;
 
 use App\Models\Video;
+use App\Services\FFProbeService;
 use Closure;
-use FFMpeg\FFProbe;
 use Illuminate\Support\Facades\Storage;
 class GetSizesVideos implements VideoUploadProcessingPipe
 {
 
-
+    /**
+     * @param Video $video
+     * @param Closure $next
+     * @return mixed
+     */
     public function handle(Video $video, Closure $next)
     {
-        $ffprobe = FFProbe::create([
-            'ffmpeg.binaries' => '/usr/bin/ffmpeg',
-            'ffprobe.binaries' => '/usr/bin/ffprobe',
-        ]);
-
-        $size_file_480 = number_format($ffprobe->format(Storage::disk('public')->path($video->settings['path_size_480']))->get('size') / 1048576, 2) . 'MB';
-        $size_file_1080 = number_format($ffprobe->format(Storage::disk('public')->path($video->settings['path_size_1080']))->get('size') / 1048576, 2) . 'MB';
-
         $video->update([
-            'settings->size_file_480'  => $size_file_480,
-            'settings->size_file_1080' => $size_file_1080,
+            'settings->size_file_480'  => number_format(FFProbeService::make()->format(Storage::disk('public')->path($video->settings['path_size_480']))->get('size') / 1048576, 2) . 'MB',
+            'settings->size_file_1080' => number_format(FFProbeService::make()->format(Storage::disk('public')->path($video->settings['path_size_1080']))->get('size') / 1048576, 2) . 'MB',
         ]);
 
         return $next($video);
